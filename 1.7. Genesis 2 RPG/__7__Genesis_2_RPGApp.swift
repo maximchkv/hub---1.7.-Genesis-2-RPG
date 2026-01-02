a3639d0c6f030f36ca2a3d612a11fc4f2da3bb4d
mxc <maximchkv@gmail.com>
2026-01-02 19:15:15 +0700

    Initial Commit

---
+++ 1.7. Genesis 2 RPG/__7__Genesis_2_RPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  __7__Genesis_2_RPGApp.swift
+//  1.7. Genesis 2 RPG
+//
+//  Created by Max on 02.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct __7__Genesis_2_RPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
